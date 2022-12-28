5f75b5e9b978154b082a666fb35290a96b451613
Crist Ye <iamCristYe@gmail.com>
2022-12-28 12:59:39 +0800

    Create redirect.js

---
+++ redirect.js
@@ -0,0 +1,16 @@
+(function () {
+    var href = window.location.href;
+
+    if (href.indexOf('.m.') > -1 || href.indexOf('/zh') > -1) {
+        window.location.replace(href.replace('.m.', '.')
+            .replace('/zh/', '/wiki/')
+            .replace('/zh-hans/', '/wiki/')
+            .replace('/zh-hant/', '/wiki/')
+            .replace('/zh-cn/', '/wiki/')
+            .replace('/zh-hk/', '/wiki/')
+            .replace('/zh-mo/', '/wiki/')
+            .replace('/zh-my/', '/wiki/')
+            .replace('/zh-sg/', '/wiki/')
+            .replace('/zh-tw/', '/wiki/'));
+    }
+})()
